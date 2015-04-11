c469f9fc0edede0ad7afd99abe2db07c1497174c
yokasheh <yokasheh@utexas.edu>
2015-04-11 12:23:03 -0500

    added all the files for the website

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    (function() {
+        [].slice.call(document.querySelectorAll('.tabs')).forEach(function(el) {
+            new CBPFWTabs(el);
+        });
+    })();
+    $('#main-nav').sidr();
+    $('#fullpage').fullpage({
+        'verticalCentered': true,
+        'easing': 'easeInOutCirc',
+        'css3': false,
+        'scrollingSpeed': 350,
+        'slidesNavigation': true,
+        'slidesNavPosition': 'bottom',
+        'easingcss3': 'ease',
+        'navigation': true,
+        'anchors': ['Home', 'Features', 'About', 'Video', 'Clients', 'Screenshots', 'Pricing', 'Download', 'Contact'],
+        'navigationPosition': 'left'
+    });
+    $('.screenshots-content, .clients-content').css('height', $(window).height());
+
+    $(document).mouseup(function (e) {
+        if ($(".sidr-open ")[0]){
+        var container = $("#sidr");
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+          $(".sidr-open #main-nav").click();
+        }}
+    });
+});
---
+++ js/signup.js
@@ -0,0 +1,71 @@
+jQuery(document).ready(function($){
+	var signUpButton = $('#sign-up-button');
+	var signUpInput = $("#signup-email-input");
+
+	var post = function(email) {
+		console.log(email);
+	};
+
+	var showThanksButton = function() {
+		signUpInput.fadeOut();
+		signUpButton.text("Thanks for signing up");
+		signUpButton.unbind("click");
+		signUpButton.css("cursor", "default");
+	};
+
+	signUpButton.on('click', function(event) {
+		var text;
+
+		if (signUpInput.is(":visible")) {
+			text = signUpInput.val();
+    		post(text.trim());
+    		showThanksButton();
+    		return;
+		}
+
+		event.preventDefault();
+		/* Act on the event */
+		// Transform button
+		signUpButton.text("Get Notified");
+
+		// Show and Bring focus to input field
+		signUpInput
+			.fadeIn()
+			.focus()
+			.putCursorAtEnd();
+
+		// Enter key action
+		signUpInput.keyup(function(e) {
+    		if (e.keyCode == 13) {
+    			// Post email
+    			var text = signUpInput.val();
+    			post(text.trim());
+    			showThanksButton();
+    		} else if (e.keyCode == 27) {
+    			signUpInput.fadeOut();
+    			signUpButton.text("Get Notified When We Launch").focus();
+    		}
+
+    	});
+ 	});
+
+
+});
+
+
+//credits http://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
+jQuery.fn.putCursorAtEnd = function() {
+	return this.each(function() {
+    	// If this function exists...
+    	if (this.setSelectionRange) {
+      		// ... then use it (Doesn't work in IE)
+      		// Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+      		var len = $(this).val().length * 2;
+      		this.setSelectionRange(len, len);
+    	} else {
+    		// ... otherwise replace the contents with itself
+    		// (Doesn't work in Google Chrome)
+      		$(this).val($(this).val());
+    	}
+	});
+};
